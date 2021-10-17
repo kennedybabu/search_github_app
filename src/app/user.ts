84f67d0a20908ea19024869d0fed7e5dcd4483b0
kennedybabu <kennedynjogu8@gmail.com>
2021-10-17 14:07:36 +0300

    generate the user class

---
+++ src/app/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    constructor(public name: string, public email: string, public bio:string, public profile_photo:string, public followers: number, public following: number){
+        
+    }
+}
